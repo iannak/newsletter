894590f2186af7029705cc8cf7633e156aba1f17
Anna Karolina Franz <franz.karols@gmail.com>
2024-01-08 02:12:34 -0300

    feat: add focus and shadow styles

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,10 +1,10 @@
 const Article = ({ title, text, tags, image, alt }) => {
   return (
-    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
+    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center sm:hover:shadow-gray-500 sm:hover:dark:shadow-gray-200  sm:hover:dark:shadow-sm">
       <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
       <div className="w-full pr-5 flex gap-2 justify-end">
         {tags.map((tag) => (
-          <span key={tag} className="px-4 py-1 bg-alura-100 dark:bg-dark-100 rounded-full text-gray-200 text-xs font-bold uppercase">
+          <span key={tag} className="px-4 py-1 bg-alura-100 dark:bg-dark-100 rounded-full text-gray-200 text-xs font-bold uppercase hover:bg-alura-200 houver:scale-110">
             {tag}
           </span>
         ))}
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,5 @@
 const Input = props => {
-    return <input className="py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none"
+    return <input className="py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none placeholder:text-gray-400 placeholder:dark:text-gray-600 placeholder:text-xs placeholder:uppercase placeholder:font-bold focus:border-2 focus:border-alura-200 focus:dark:border-alura-100 invalid:focus:border-2 invalid:focus:border-red-400 dark:focus:invalid:border-2 dark:focus:invalid:border-red-400"
         {...props} />
 }
 
