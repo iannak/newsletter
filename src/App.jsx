ddcd0aa1184c149ab7038f2e1735abf1b6f3a775
Anna Karolina Franz <franz.karols@gmail.com>
2024-01-08 00:34:05 -0300

    feat: add content and stylized with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ArticleList from './components/ArticleList/ArticleList'
 import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
       <Header />
+      <ArticleList />
     </>
   )
 }
